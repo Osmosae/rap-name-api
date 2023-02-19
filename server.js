1349fbda7f973c6fa942795000015f3406ec24e6
Sean Quill <osmosae@gmail.com>
2023-02-19 09:56:38 +0000

    working rapper api

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    "21 savage": {
+        age: 29,
+        birthName: "Shéyaa Bin Abraham-Joseph",
+        birthLocation: "London, England",
+    },
+    "chance the rapper": {
+        age: 29,
+        birthName: "Chancelor Johnathan Bennett",
+        birthLocation: "Chicago, Illinois",
+    },
+    unknown: {
+        age: 0,
+        birthName: "Unknown",
+        birthLocation: "UnKnown",
+    },
+}
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get("/api/:name", (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]) {
+        res.json(rappers[rapperName])
+    } else {
+        res.json(rappers["unknown"])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on locahost:${PORT} ........ You better go catch it!`)
+})
